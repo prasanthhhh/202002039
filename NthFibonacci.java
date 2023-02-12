74c3e29ebb1e0840480cafe4835f19ab2d886bce
whoisUjjwal <110911377+Kodiererin@users.noreply.github.com>
2023-02-12 13:39:43 +0530

    Update NthFibonacci.java
    Updated Some efficient Codes to this problem.

--- NthFibonacci.java
+++ NthFibonacci.java
@@ -10,4 +10,19 @@ public class NthFibonacci {
 
         return first;
     }
+//     Another Solution of this problem
+     static int factorial(int n)
+    {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+//     Solution 2
+    int factorial(int n)
+    {
+ 
+        // single line to find factorial
+        return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);
+    }
 }
